5cb445edbb33e88bbcb3d0f7a5b02dc8bb0211c0
Meliso Einsfar <einsfar@gmail.com>
2021-10-01 13:12:40 -0700

    Python Hello World

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse  
+  
+def hello(request):  
+    return HttpResponse("<h2>Hello, Welcome to Django!</h2>")  
